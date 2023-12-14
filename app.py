dc6c307d3a4eae04f19e876caf5723d9edc86cb0
pfbrodon <pfbrodon@gmail.com>
2023-12-14 01:18:37 -0300

    MAC

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, url_for,redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Numeric
 app = Flask(__name__)
 
 
 # configuro la base de datos, con el nombre el usuario y la clave
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost/proyecto'# MAC OS
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:12345678@localhost/proyecto'# MAC OS
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #none
 db= SQLAlchemy(app)   #crea el objeto db de la clase SQLAlquemyb ,cvgb                                    
 
@@ -28,6 +28,8 @@ class Producto(db.Model):
         self.precioUnit=precioUnit
         self.precioVPublico=precioVPublico
         
+###################################################################
+        
 @app.route('/')
 def index():
     # Obtén los datos de la tabla
@@ -36,11 +38,54 @@ def index():
 
 ###################################################################
 
-@app.route('/editar/<int:id>')
+@app.route('/editar/<int:id>', methods=['POST','GET'])
 def editar(id):
-    # Obtén los datos del elemento con el ID proporcionado
     item = Producto.query.get(id)
+    if request.method=='POST':
+        item.cantidad=request.form.get('cantidad')
+        item.categoria=request.form.get('categoria')
+        item.codigo=request.form.get('codigo')
+        item.descripcion=request.form.get('descripcion')
+        item.precioUnit=request.form.get('precioUnit')
+        item.precioVPublico=request.form.get('precioVPublico')
+        db.session.commit() # confirma el alta
+        return redirect(url_for('index'))
+    # Obtén los datos del elemento con el ID proporcionado
     return render_template('editar.html', item=item)
+
+###################################################################
+
+@app.route('/nuevo', methods=['POST','GET']) # crea ruta o endpoint
+def nuevo():
+    if request.method=='POST':        #print(request.json)  # request.json contiene el json que envio el cliente
+        cantidad=request.form.get('cantidad')
+        categoria=request.form.get('categoria')
+        codigo=request.form.get('codigo')
+        descripcion=request.form.get('descripcion')
+        precioUnit=request.form.get('precioUnit')
+        precioVPublico=request.form.get('precioVPublico')
+        productoNuevo=Producto(cantidad,categoria,codigo,descripcion,precioUnit,precioVPublico)
+        db.session.add(productoNuevo)
+        db.session.commit() # confirma el alta
+        return redirect(url_for('index'))
+    return render_template('nuevo.html')
+
+###################################################################
+
+@app.route('/eliminar/<int:id>',methods=['POST','GET'])
+def eliminar(id):
+    item=Producto.query.get(id)
+    if request.method=='POST':
+        db.session.delete(item)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return redirect(url_for('index'))
+
+    
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
